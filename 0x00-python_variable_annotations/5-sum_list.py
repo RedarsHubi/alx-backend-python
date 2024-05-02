0abd4c394f736236faa26bd833a20e5f656e772e
RedarsHubi <redamasters@gmail.com>
2024-05-02 14:25:59 +0100

    Update type annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Typed function sum_list"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Takes float list returns float"""
     sum: float = 0
     for e in input_list:
