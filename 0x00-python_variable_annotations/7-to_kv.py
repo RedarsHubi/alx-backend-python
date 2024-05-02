a10ff1fcf8b1443285fbbadd8861f052d18472ac
RedarsHubi <redamasters@gmail.com>
2024-05-02 18:04:58 +0100

    .

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Complex type function to_kv"""
-from typing import Tuple, Union
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
