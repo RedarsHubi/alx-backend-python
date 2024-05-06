02d3a891796b31a6ef203eff50430d45bba2d2b4
RedarsHubi <redamasters@gmail.com>
2024-05-06 12:41:48 +0100

    Fixed order (tasks)

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Async coroutine wait_n"""
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -9,7 +8,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays"""
-    lista = []
-    for e in range (n):
-        lista.append(await wait_random(max_delay))
-    return lista
+    preps = [asyncio.create_task(wait_random(max_delay)) for e in range (n)]
+    return [await prep for prep in asyncio.as_completed(preps)]
