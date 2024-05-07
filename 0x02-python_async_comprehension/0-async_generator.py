e9cbcdac3e601b498a97fbe7f4fac2360fe4e217
RedarsHubi <redamasters@gmail.com>
2024-05-07 14:43:48 +0100

    .

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async coroutine async_generator"""
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """returns random list of floats"""
     for _ in range(10):
         await asyncio.sleep(1)
